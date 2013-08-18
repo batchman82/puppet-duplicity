b405e335b620e5d89782aea08ff16bf3502a8178
Markus Liljergren <markus@kloudberry.co.uk>
2013-08-18 08:32:59 -0500

    Just removed unnecessary spaces

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,15 +1,15 @@
 # Duplicity::Params TODO: Write a good definition
 class duplicity::params(
-  $bucket                = undef,
-  $dest_id               = undef,
-  $dest_key              = undef,
-  $cloud                 = $duplicity::defaults::cloud,
-  $pubkey_id             = undef,
-  $hour                  = $duplicity::defaults::hour,
-  $minute                = $duplicity::defaults::minute,
-  $full_if_older_than    = $duplicity::defaults::full_if_older_than,
-  $remove_older_than     = undef,
-  $job_spool             = $duplicity::defaults::job_spool
+  $bucket             = undef,
+  $dest_id            = undef,
+  $dest_key           = undef,
+  $cloud              = $duplicity::defaults::cloud,
+  $pubkey_id          = undef,
+  $hour               = $duplicity::defaults::hour,
+  $minute             = $duplicity::defaults::minute,
+  $full_if_older_than = $duplicity::defaults::full_if_older_than,
+  $remove_older_than  = undef,
+  $job_spool          = $duplicity::defaults::job_spool
 ) inherits duplicity::defaults {
 
   file { $job_spool :
